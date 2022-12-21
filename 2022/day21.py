c202d8422384d0f5dcc7645352f4e74d2df147d8
RKuipers <robin_kuipers93@hotmail.com>
2022-12-21 09:46:01 +0000

    Fixed types

--- 2022/day21.py
+++ 2022/day21.py
@@ -24,14 +24,14 @@ def d21p1(file) -> int:
         x = d[n]
         l = x[0]
         r = x[2]
-        if type(d[l]) == type(1) and type(d[r]) == type(1):
+        if type(d[l]) == int and type(d[r]) == int:
             d[n] = int(ops[x[1]](d[l], d[r]))
             to_eval = to_eval[:-1]
             continue
         
-        if type(d[l]) == type([]) and l not in to_eval:
+        if type(d[l]) == list and l not in to_eval:
             to_eval.append(l)
-        if type(d[r]) == type([]) and r not in to_eval:
+        if type(d[r]) == list and r not in to_eval:
             to_eval.append(r)
     
     return d["root"]
@@ -60,19 +60,19 @@ def d21p2(file) -> int:
         l = x[0]
         r = x[2]
         
-        if type(d[l]) == type(1) and type(d[r]) == type(1):
+        if type(d[l]) == int and type(d[r]) == int:
             d[n] = int(ops[x[1]](d[l], d[r]))
             to_eval = to_eval[:-1]
             continue    
         
-        if (type(d[l]) == type("") or type(d[l]) == type(1)) and (type(d[r]) == type(1) or type(d[r]) == type("")):
+        if (type(d[l]) == str or type(d[l]) == int) and (type(d[r]) == int or type(d[r]) == str):
             d[n] = f"({d[l]} {x[1]} {d[r]})"
             to_eval = to_eval[:-1]
             continue   
         
-        if type(d[l]) == type([]):
+        if type(d[l]) == list:
             to_eval.append(l)
-        if type(d[r]) == type([]):
+        if type(d[r]) == list:
             to_eval.append(r)
     
     humn = symbols('humn')
