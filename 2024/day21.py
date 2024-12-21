f98c13cff38cebe71e8053656ca29c7139161f92
RKuipers <robin.kuipers@moixa.com>
2024-12-21 11:24:47 +0000

    Day 21

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,36 +1,29 @@
 from functools import cache
 import random
 import time
-from utils import Coord, MAXVAL
 
 number_keypad = {
-    7: Coord(0, 0),
-    8: Coord(1, 0),
-    9: Coord(2, 0),
-    4: Coord(0, 1),
-    5: Coord(1, 1),
-    6: Coord(2, 1),
-    1: Coord(0, 2),
-    2: Coord(1, 2),
-    3: Coord(2, 2),
-    0: Coord(1, 3),
-    -1: Coord(2, 3),
+    "7": 0,
+    "8": 1,
+    "9": 2,
+    "4": 1j,
+    "5": 1 + 1j,
+    "6": 2 + 1j,
+    "1": 2j,
+    "2": 1 + 2j,
+    "3": 2 + 2j,
+    " ": 3j,
+    "0": 1 + 3j,
+    "A": 2 + 3j,
 }
 
 direction_keypad = {
-    Coord(0, -1): Coord(1, 0),
-    -1: Coord(2, 0),
-    Coord(-1, 0): Coord(0, 1),
-    Coord(0, 1): Coord(1, 1),
-    Coord(1, 0): Coord(2, 1),
-}
-
-stringify_direction_keypad = {
-    Coord(0, -1): "^",
-    -1: "A",
-    Coord(-1, 0): "<",
-    Coord(0, 1): "v",
-    Coord(1, 0): ">",
+    " ": 0,
+    "^": 1,
+    "A": 2,
+    "<": 1j,
+    "v": 1 + 1j,
+    ">": 2 + 1j,
 }
 
 
@@ -38,7 +31,7 @@ def parseA(file):
     with open(file) as f:
         lines = f.readlines()
 
-    return [list(map(int, l.strip()[:-1])) + [-1] for l in lines]
+    return [l.strip() for l in lines]
 
 
 def parseB(file):
@@ -46,66 +39,72 @@ def parseB(file):
 
 
 @cache
-def get_keys(start, end, deadspace):
+def get_keys(start, end):
+    pad = (
+        number_keypad
+        if (start in number_keypad and end in number_keypad)
+        else direction_keypad
+    )
+
     if start == end:
-        return [-1]
+        return "A"
 
-    m = end - start
+    m = pad[end] - pad[start]
 
-    if m.y == 0:
-        return abs(m.x) * [Coord(m.x // abs(m.x), 0)] + [-1]
-    elif m.x == 0:
-        return abs(m.y) * [Coord(0, m.y // abs(m.y))] + [-1]
+    if m.imag == 0:
+        return int(-m.real) * "<" + int(m.real) * ">" + "A"
+    elif m.real == 0:
+        return int(-m.imag) * "^" + int(m.imag) * "v" + "A"
+
     y_first = random.random() < 0.5
-    if Coord(start.x + m.x, start.y) == deadspace:
+    if pad[start] + m.real == pad[" "]:
         y_first = True
-    elif Coord(start.x, start.y + m.y) == deadspace:
+    elif pad[start] + m.imag * 1j == pad[" "]:
         y_first = False
+
     if y_first:
         return (
-            abs(m.y) * [Coord(0, m.y // abs(m.y))]
-            + abs(m.x) * [Coord(m.x // abs(m.x), 0)]
-            + [-1]
+            int(-m.imag) * "^"
+            + int(m.imag) * "v"
+            + int(-m.real) * "<"
+            + int(m.real) * ">"
+            + "A"
         )
     else:
         return (
-            abs(m.x) * [Coord(m.x // abs(m.x), 0)]
-            + abs(m.y) * [Coord(0, m.y // abs(m.y))]
-            + [-1]
+            int(-m.real) * "<"
+            + int(m.real) * ">"
+            + int(-m.imag) * "^"
+            + int(m.imag) * "v"
+            + "A"
         )
 
 
+@cache
+def get_path(code, depth):
+    v = 0
+    if depth == 0:
+        return len(code)
+    for i, c in enumerate(code):
+        v += get_path(get_keys(code[i - 1], c), depth - 1)
+    return v
+
+
 def solve(code, depth):
-    min_path = MAXVAL
-    for _ in range(10):
+    min_path = -1
+    for _ in range(1000):
         get_keys.cache_clear()
-        path = []
-
-        prev = -1
-        for i, c in enumerate(code):
-            path += get_keys(number_keypad[prev], number_keypad[c], Coord(0, 3))
-            prev = c
-
-        for iteration in range(depth):
-            new_path = []
-            prev = -1
-            for i, c in enumerate(path):
-                new_path += get_keys(
-                    direction_keypad[prev],
-                    direction_keypad[c],
-                    Coord(0, 0),
-                )
-                prev = c
-            path = new_path
-        if len(path) < min_path:
-            min_path = len(path)
+        get_path.cache_clear()
+        path = get_path(code, depth)
+        if path < min_path or min_path == -1:
+            min_path = path
     return min_path
 
 
 def d21a(parsed):
     complexities = []
     for code in parsed:
-        min_path = solve(code, 2)
+        min_path = solve(code, 3)
         complexities.append(int("".join(list(map(str, code[:-1])))) * min_path)
     return sum(complexities)
 
@@ -113,7 +112,7 @@ def d21a(parsed):
 def d21b(parsed):
     complexities = []
     for code in parsed:
-        min_path = solve(code, 25)
+        min_path = solve(code, 26)
         complexities.append(int("".join(list(map(str, code[:-1])))) * min_path)
     return sum(complexities)
 
