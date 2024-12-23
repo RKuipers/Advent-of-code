806462aac43a4018ded346130657fcb99a4bf110
RKuipers <robin.kuipers@moixa.com>
2024-12-23 13:28:37 +0000

    Day 23

--- 2024/day22.py
+++ 2024/day22.py
@@ -1,21 +1,62 @@
+from functools import cache
 import time
 from utils import Coord
 
+@cache
+def next_code(x):
+    x = (x ^ (x * 64)) % 16777216
+    x = (x ^ (x // 32)) % 16777216
+    x = (x ^ (x * 2048)) % 16777216
+    return x
 
 def parseA(file):
-    pass
+    with open(file) as f:
+        lines = f.readlines()
+
+    return list(map(int, lines))
 
 
 def parseB(file):
     return parseA(file)
 
 
-def d22a(parsed):
-    pass
+def d22a(init_numbers):
+    final_numbers = []
+    for n in init_numbers:
+        x = n
+        for _ in range(2000):
+            x = next_code(x)
+        final_numbers.append(x)
+    return sum(final_numbers)
 
 
-def d22b(parsed):
-    pass
+def d22b(init_numbers):
+    changes = {}
+    values = {}
+    all_codes = set()
+    code_values = {}
+    for n in init_numbers:
+        changes[n] = []
+        values[n] = {}
+        x = n
+        for _ in range(2000):
+            x_new = next_code(x)
+            changes[n].append((x_new % 10) - (x % 10)) 
+            x = x_new   
+
+            code = tuple(changes[n][-4:])
+
+            if len(code) == 4 and not code in values[n].keys():
+                values[n][code] = x_new % 10
+                all_codes.add(code)
+
+    for code in all_codes:
+        code_values[code] = sum(v.get(code, 0) for v in values.values())
+
+    max_code = max(code_values, key=code_values.get)
+
+    return max_code, code_values[max_code]
+                
 
 
 testfile = "2024/inputs/day22testinput.txt"
--- 2024/day23.py
+++ 2024/day23.py
@@ -2,20 +2,69 @@ import time
 from utils import Coord
 
 
-def parseA(file):
-    pass
+def parseA(file):    
+    with open(file) as f:
+        lines = f.readlines()
+
+    return [tuple(l.strip().split("-")) for l in lines]
 
 
 def parseB(file):
     return parseA(file)
 
 
-def d23a(parsed):
-    pass
+def d23a(edges):
+    t_nodes = {}
+    for e in edges:
+        if e[0][0] == "t":
+            if e[0] in t_nodes.keys():
+                t_nodes[e[0]].append(e[1])
+            else:
+                t_nodes[e[0]] = [e[1]]
+        elif e[1][0] == "t":
+            if e[1] in t_nodes.keys():
+                t_nodes[e[1]].append(e[0])
+            else:
+                t_nodes[e[1]] = [e[0]]
+    
+    count = 0
+    for t_node, nodes in t_nodes.items():
+        for i, n in enumerate(nodes):
+            for j, n2 in enumerate(nodes[i:]):
+                if (n, n2) in edges or (n2, n) in edges:
+                    count += 1
+    return count
 
+def bron_kerbosch(r, p, x, neighbors):
+    if len(p) == 0 and len(x) == 0:
+        return r
+    n_neighs = {u: len(neighbors[u]) for u in p.union(x)}
+    u = min(n_neighs, key=n_neighs.get)
+    nu = neighbors[u]
+    max_clique = set()
+    for v in p.difference(nu):
+        nv = neighbors[v]
+        new_clique = bron_kerbosch(r.union({v}), p.intersection(nv), x.intersection(nv), neighbors)
+        if len(new_clique) > len(max_clique):
+            max_clique = new_clique
+        p.remove(v)
+        x.add(v)
+    return max_clique
+    
 
-def d23b(parsed):
-    pass
+def d23b(edges):
+    neighbors = {}
+    for e in edges:
+        if e[0] in neighbors.keys():
+            neighbors[e[0]].add(e[1])
+        else:
+            neighbors[e[0]] = {e[1]}        
+        if e[1] in neighbors.keys():
+            neighbors[e[1]].add(e[0])
+        else:
+            neighbors[e[1]] = {e[0]}
+    max_clique = bron_kerbosch(set(), set(neighbors.keys()), set(), neighbors)
+    return ",".join(sorted(list(max_clique)))
 
 
 testfile = "2024/inputs/day23testinput.txt"
